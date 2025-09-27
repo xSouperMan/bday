fe9738302dca3e86cc69e399bfdc6f4851f3c8ac
SouperMan <43967698+xSouperMan@users.noreply.github.com>
2025-09-27 10:34:46 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function submit(str) {
+    const code = ""
+    if(str != null) {
+        if(str == code) {
+            alert("yay")
+        } else {
+            //falscher code
+        }
+    }
+}
